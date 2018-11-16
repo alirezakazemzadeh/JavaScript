77f171e1395395792063dd49714f40c4a052d425
alirezakazemzadeh <44926625+alirezakazemzadeh@users.noreply.github.com>
2018-11-16 20:48:33 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Lecture Variables
+
+/*
+console.log("Hello World");
+
+var name = 'Alireza';
+console.log(name);
+
+var lastName = 'Kazemzadeh';
+console.log(lastName);
+
+var age = 33;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: Variables 2
+/*
+var name = 'Alireza';
+var age = 33;
+
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'co-founder';
+isMarried = true;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.')
+
+job = "Driver";
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.')
+
+
+//var lastName = prompt('What is your last name?');
+//console.log(lastName);
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+//Lecture Operators
+var now = 2018;
+var birthYear = now - 33;
+
+birthYear
+console.log(birthYear);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
